74e04c176e1c0bb0e4841ed6b42e28bf2634ad06
Axel L Ornelas M <axel.lom17@gmail.com>
2021-09-30 04:30:43 -0700

    Se calcula el puntaje final y se muestran los valores en la interfaz

--- app/src/main/java/uabc/axel/ornelas/juegodedados/Fila.kt
+++ app/src/main/java/uabc/axel/ornelas/juegodedados/Fila.kt
@@ -1,40 +1,54 @@
 package uabc.axel.ornelas.juegodedados
 
+import android.graphics.Color
 import android.widget.Button
 import android.widget.TextView
-import java.lang.NumberFormatException
 
 class Fila(val puntajes: Array<TextView>, val botonAccion: Button) {
-    var nJugadas: Int = 0
+    var nJugada: Int = 0
+
     //Puntaje que se puede realizar para la ronda
     var puntajeRonda: Int = 0
-    var puntajeFila: Int = 0
 
+    var puntajeFila: Int = 0
+        private set
     init {
-        botonAccion.setOnClickListener {
-            calcularPuntajeFila()
-        }
+        puntajes[puntajes.lastIndex].setTextColor(Color.MAGENTA)
     }
 
     /**
-     *
+     * Calcula el puntaje total de la fila solo sumando el ultimo valor
      */
     fun calcularPuntajeFila() {
-        try {
-            puntajeFila = puntajes.sumOf { it.text.toString().toInt() }
-            puntajes[puntajes.lastIndex].text = puntajeFila.toString()
-        } catch (e: NumberFormatException) {
-            println("No hay numero")
-        }
+        //Obtengo el número de la jugada actual
+        val puntajeTexto = puntajes[nJugada]
+        // Obtengo el texto del puntaje
+        val texto = puntajeTexto.text.toString()
+        //Sumo el puntaje de la fila con el nuevo
+        puntajeFila += if (texto == "" || texto == "/") 0 else texto.toInt()
+        puntajeTexto.setTextColor(Color.BLUE)
+        //Si la jugada no tiene valor se le pone un palo
+        if (puntajeTexto.text == "")
+            puntajeTexto.text = "/"
+        //Pone el resultado total de la suma al final
+        puntajes[puntajes.lastIndex].text = puntajeFila.toString()
+        // Se desactiva el boton
+        botonAccion.isEnabled = false
+        //Se empieza de nuevo la ronda
+        puntajeRonda = 0
+        // Se incrementa la jugada
+        nJugada++
     }
 
     /**
-     *
+     * Se asignan los valores por defecto, se usa al reiniciar el juego
      */
     fun valoresDefecto() {
-        nJugadas = 0
+        nJugada = 0
         puntajeRonda = 0
         puntajeFila = 0
+        for (puntaje in puntajes)
+            puntaje.text = ""
     }
 
 }
