656067c88b044f0419598279124c195a29ddda7f
Axel L Ornelas M <axel.lom17@gmail.com>
2021-09-29 17:57:59 -0700

    Se creo el archivo donde contendra las reglas

---
+++ app/src/main/java/uabc/axel/ornelas/juegodedados/Reglas.kt
@@ -0,0 +1,11 @@
+package uabc.axel.ornelas.juegodedados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reglas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reglas)
+    }
+}
