9f0b59d339759185af6f7d44b35720cd68a0ede2
Axel L Ornelas M <axel.lom17@gmail.com>
2021-10-01 01:08:07 -0700

    Clase para el tablero

---
+++ app/src/main/java/uabc/axel/ornelas/juegodedados/Tablero.kt
@@ -0,0 +1,11 @@
+package uabc.axel.ornelas.juegodedados
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Tablero : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tablero)
+    }
+}
